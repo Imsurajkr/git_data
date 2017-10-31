d35420a89087e63a463dfa3877b33616dc4ae11f
Suraj Kumar <infosurajkumard@gmail.com>
2017-10-31 03:17:07 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
+                "this is something"
             )
         raise
     execute_from_command_line(sys.argv)
